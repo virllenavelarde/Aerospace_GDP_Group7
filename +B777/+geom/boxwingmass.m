1e5ea1f92a1ea03bbe571e58600d5f73b88bd341
Philip501st <philip.kv04@gmail.com>
2026-03-10 17:44:51 +0000

    Update boxwingmass.m
    Added code to calcaulte the wing planform parameters

--- +B777/+geom/boxwingmass.m
+++ +B777/+geom/boxwingmass.m
@@ -60,6 +60,72 @@ function [m_boxwing] = raymerWingMass(obj)
     t_wf = 0.15*c_rf*SI.ft;      % Thickness of root (front wing), with 15% of root chord [ft] 
     t_wr = 0.15*c_rr*SI.ft;      % [ft]
 
+     %% calculate geometry  - follows the same process as Fintan's code for wing.m file
+    
+    c_tf = tr*c_rf;              %Tip chord for front wing [m]
+    c_tr = tr*c_rr;              %Tip chord for rear wing [m]
+
+    ys_f = [-bf/2; 0; bf/2];     %Each front wing section: left tip,centroid, right tip i assign a ys position
+    ys_r = [-br/2; 0; br/2];     %Each rear wing section: left tip,centroid, right tip i assign a ys position
+    
+    cs_f = [c_tf; c_rf; c_tf];   %this represents the chord distribution over the front wing so it goes from lefttip>rootchord>righttip
+    cs_r = [c_tr; c_rr; c_tr ];   %this represents the chord distribution over the rear wing so it goes from lefttip>rootchord>righttip
+    
+    x_le_f = [tand(SweepQtrChord)*(bf/2); 0; tand(SweepQtrChord)*(bf/2)]; %front wing leading edge x locations
+    x_le_r = [-tand(SweepQtrChord)*(br/2); 0; -tand(SweepQtrChord)*(br/2)]; %rear wing leading edge x locations
+
+    x_le_f = x_le_f - 0.25*c_rf; %move the above x locations to quater chord
+    x_le_r = x_le_r - 0.25*c_rr; %move the above x locations to quater chord
+
+
+    x_qtr_f = x_le_f + 0.25*cs_f; %x position of the quater chord line for the front wing
+    x_te_f  = x_le_f + cs_f;      %x position of the trailing edge for the front wing
+    
+    x_qtr_r = x_le_r + 0.25*cs_r; %x position of the quater chord line for the rear wing
+    x_te_r  = x_le_r + cs_r;      %x position of the trailing edge for the rear wing
+    
+
+    %Combine x and y coordinates to create closed planform outlines for the front and rear wings
+    Xs_f = [x_le_f, ys_f; flipud(x_te_f), flipud(ys_f)];
+    Xs_r = [x_le_r, ys_r; flipud(x_te_r), flipud(ys_r)];
+
+    %CALCULATED AERODYNAMIC CHORD below up till line 108
+    % taper ratios
+    lambda_f = c_tf / c_rf;
+    lambda_r = c_tr / c_rr;
+    
+
+    % mean aerodynamic chord lengths
+    c_mac_f = (2/3) * c_rf * ((1 + lambda_f + lambda_f^2) / (1 + lambda_f));
+    c_mac_r = (2/3) * c_rr * ((1 + lambda_r + lambda_r^2) / (1 + lambda_r));
+
+    % spanwise MAC locations from aircraft centreline
+    y_mac_f = (bf/6) * ((1 + 2*lambda_f) / (1 + lambda_f));
+    y_mac_r = (br/6) * ((1 + 2*lambda_r) / (1 + lambda_r));
+
+    % local aerodynamic-centre x-position before aircraft placement
+    x_ac_f_local = interp1(ys_f(2:3), x_qtr_f(2:3), y_mac_f);
+    x_ac_r_local = interp1(ys_r(2:3), x_qtr_r(2:3), y_mac_r);
+
+    % place aerodynamic centre at desired aircraft x-position
+    Xs_f(:,1) = Xs_f(:,1) + (obj.FrontWingPos - x_ac_f_local);
+    Xs_r(:,1) = Xs_r(:,1) + (obj.RearWingPos  - x_ac_r_local);
+    
+    x_ac_f = obj.FrontWingPos;
+    x_ac_r = obj.RearWingPos;
+
+
+    % equivalent overall reference values (basically just an average wing positions for empenage_bw.m)
+    obj.c_ac = alpha*c_mac_f + (1-alpha)*c_mac_r;
+    obj.x_ac = alpha*x_ac_f + (1-alpha)*x_ac_r;
+    obj.WingPos = obj.x_ac;
+    obj.WingArea = Sf + Sr;
+
+    GeomObj(1) = cast.GeomObj(Name="Front Wing", Xs=Xs_f);
+    GeomObj(2) = cast.GeomObj(Name="Rear Wing",  Xs=Xs_r);
+
+    
+
     % ----- compute each wing panel mass using Raymer helper -----
     w_front_lb = 0.00125*Wdg_f_lb * (b_f_ft/cosLambda)^0.75*...
         (1 + sqrt(6.3*cosLambda/b_f_ft))*n_z^0.55*...
@@ -104,4 +170,5 @@ end
 %     %calc total area
 %     S = 2*(A1+A2+A3);
 % end
+
 
